3198b3562b3975fcb38cabf58482046bf3c250e6
shamail786 <haider.shamail68@gmail.com>
2023-08-05 14:35:34 +1000

    Added 10 more terms

--- routes/index.js
+++ routes/index.js
@@ -81,6 +81,86 @@ const nameList = [
     References:
       "Jianhao Cao, Tamara Munzner, Rachel Pottinger,Visualizing a Tabular Data Repository to Facilitate Descriptive Tag Augmentation for New Tables. HILDA '23: Proceedings of the Workshop on Human-In-the-Loop Data AnalyticsJune 2023Article No.: 10Pages 1,3. ",
   },
+  {
+    id: 11,
+    Term: "Minimum Viable Product",
+    Description:
+      "The notion of a Minimum Viable Product (MVP) is used in product development, notably in the context of agile and lean approaches. It refers to the most basic version of a product that may be produced and distributed in order to address the primary demands of early adopters or target consumers.",
+    References:
+      "Keogh, C. (2022) ‘Minimum viable product (MVP)’, Definitions,10th March 2022 ",
+  },
+  {
+    id: 12,
+    Term: "Continuous Integration",
+    Description:
+      "Continuous Integration (CI) is a software development practice that involves integrating code changes from multiple developers into a shared repository frequently, typically several times a day.",
+    References:
+      "Henning, A. (2016) ‘Physical continuous integration — CI on real robots!’, ROSCon Seoul 2016 [Preprint].",
+  },
+  {
+    id: 13,
+    Term: "GitOps",
+    Description:
+      "All infrastructure and application modifications are managed using version-controlled Git repositories.",
+    References:
+      "Bowersox, K. (2021) Gitops Foundations. Erscheinungsort nicht ermittelbar: LinkedIn.  ",
+  },
+  {
+    id: 14,
+    Term: "Microservices",
+    Description:
+      "A design strategy in which big applications are divided into smaller, loosely linked services that may be created, deployed, and scaled separately.",
+    References:
+      "Christudas, B. (2019) ‘Introducing microservices’, Practical Microservices Architectural Patterns, pp. 21–34.",
+  },
+  {
+    id: 15,
+    Term: "Docker",
+    Description:
+      "Docker is an open-source platform that allows developers to use containerization to automate the deployment, scaling, and administration of applications. It provides a platform for developers to bundle their programmes and dependencies into standardised containers. Containers enable programmes to execute consistently and reliably across several environments, making software development, testing, and deployment easier.",
+    References:
+      "Jangla, K. (2018) ‘Docker Basics’, Accelerating Development Velocity Using Docker, pp. 27–53. ",
+  },
+  {
+    id: 16,
+    Term: "Continuous Deployment",
+    Description:
+      "Continuous Deployment (CD) is an extension of Continuous Integration (CI) and an important DevOps practise. It refers to the automated process of frequently and reliably delivering code changes from development to production environments. ",
+    References:
+      "‘DevOps: An overview’ (2017) The DevOps Adoption Playbook, pp. 1–38. ",
+  },
+  {
+    id: 17,
+    Term: "Versioning",
+    Description:
+      "The process of assigning unique identifiers (version numbers or tags) to code, documents, or artifacts to track changes and ensure consistency.",
+    References:
+      "Bowersox, K. (2021) Gitops Foundations. Erscheinungsort nicht ermittelbar: LinkedIn. ",
+  },
+  {
+    id: 18,
+    Term: "Behaviour Driven Design",
+    Description:
+      "Behavior-Driven Design (BDD) is a software development process that builds on the ideas of Test-Driven Development (TDD) by concentrating on the product's behaviour and consequences from the perspective of business stakeholders. ",
+    References:
+      "DevOps: From newbie to Professiona: Fast and simple guide to devops (2016). CreateSpace Independent Publishing Platform.  ",
+  },
+  {
+    id: 19,
+    Term: "Test Driven Development",
+    Description:
+      "Test-Driven Development is a software development approach that emphasises developing tests before producing code. The procedure entails generating automated tests iteratively to specify the expected behaviour of the programme and then writing code to pass those tests. ",
+    References:
+      "DevOps: From newbie to Professiona: Fast and simple guide to devops (2016). CreateSpace Independent Publishing Platform. ",
+  },
+  {
+    id: 20,
+    Term: "Cross-functional autonomous teams ",
+    Description:
+      "The notion of cross-functional autonomous teams is central to DevOps and Agile techniques. These teams are made up of people with a variety of skills and expertise who work together to develop software products or services. ",
+    References:
+      "Meyer, B. (2014) Agile!: The good, the hype and the ugly. Zurich: Springer International Publishing.  ",
+  },
 ];
 
 /* GET home page. */
